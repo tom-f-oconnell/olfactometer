c2c80f48ba4295166fba136c99873db1b1c01de4
Tom O'Connell <toconnel@caltech.edu>
2020-08-07 01:45:40 -0700

    Add flag to not upload, copied some of example nanopb code into sketch but not working (still not linking right), TODOs

--- firmware/olfactometer/olfactometer.ino
+++ firmware/olfactometer/olfactometer.ino
@@ -15,7 +15,44 @@
 #include <pb_decode.h>
 #include "olf.pb.h"
 
+// TODO check this actually would be defined here w/ some other things we
+// expect to be enabled
+#ifdef PB_ENABLE_MALLOC
+#error I don't want malloc enabled.
+#endif
+
+uint8_t buffer[128];
+bool status;
+
 void setup() {
+    Serial.begin(9600);
+
+    /* Allocate space for the decoded message. */
+    PulseTiming message = PulseTiming_init_zero;
+    
+    // TODO if message_length isn't required, how am i supposed to call this?
+    // TODO TODO can i just sent the bufsize to the length of buffer?
+    // (> length of actual message in most cases)?
+    /* Create a stream that reads from the buffer. */
+    //pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
+    pb_istream_t stream = pb_istream_from_buffer(buffer, 128);
+    
+    /* Now we are ready to decode the message. */
+    status = pb_decode(&stream, PulseTiming_fields, &message);
+    
+    /* Check for errors... */
+    if (!status)
+    {
+        //printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
+        //return 1;
+        Serial.print("Decoding failed: ");
+        Serial.println(PB_GET_ERROR(&stream));
+    }
+    
+    /* Print the data contained in the message. */
+    //printf("Your lucky number was %d!\n", (int)message.lucky_number);
+    Serial.print("pre_pulse_us: ");
+    Serial.println((unsigned long) message.pre_pulse_us);
 }
 
 void loop() {
--- upload.py
+++ upload.py
@@ -4,6 +4,7 @@ from subprocess import Popen
 from os.path import join, splitext, exists, abspath
 import glob
 import shutil
+import argparse
 
 
 sketch_dir = 'firmware/olfactometer'
@@ -29,7 +30,7 @@ def generate_and_move_protobuf_code():
         shutil.move(f, dest)
 
 
-def upload(board='arduino:avr:mega', port='/dev/ttyACM0'):
+def upload(board='arduino:avr:mega', port='/dev/ttyACM0', dry_run=False):
     # This does need to be absolute
     build_path = abspath('build')
     if exists(build_path):
@@ -41,27 +42,40 @@ def upload(board='arduino:avr:mega', port='/dev/ttyACM0'):
     if exists(build_cache_path):
         shutil.rmtree(build_cache_path)
 
-    cmd = (f'arduino-cli compile -b {board} -u -t -p {port} -v {sketch_dir} '
+    cmd = (f'arduino-cli compile -b {board} -v {sketch_dir} '
         f'--build-path {build_path} '
         '--build-properties compiler.c.extra_flags=-Inanopb '
         '--build-properties compiler.cpp.extra_flags=-Inanopb'
     )
+    upload_args = f' -u -t -p {port}'
+    if not dry_run:
+        cmd += upload_args
+
     print(cmd)
     # TODO parse output to check the -t flag indicated successful verification
     p = Popen(cmd, shell=True)
     p.communicate()
 
-    # TODO test these don't err if compilation errs (would compilation err raise
-    # anyway?)
-    shutil.rmtree(build_path)
-    shutil.rmtree(build_cache_path)
+    #'''
+    if exists(build_path):
+        shutil.rmtree(build_path)
+
+    if exists(build_cache_path):
+        shutil.rmtree(build_cache_path)
+    #'''
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dry-run', action='store_true', default=False,
+        help='do not actually upload. just compile.'
+    )
+    args = parser.parse_args()
+    
     generate_and_move_protobuf_code()
     # TODO replace w/ passing appropriate options to arduino so it can use the
     # files in situ without having to copy them
-    upload()
+    upload(dry_run=args.dry_run)
 
 
 if __name__ == '__main__':
